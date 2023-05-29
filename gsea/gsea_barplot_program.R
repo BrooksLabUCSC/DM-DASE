767f4c8b0958123d6d4bfe087c5f4f8485b88cf2
Carlos Arevalo <46701163+caeareva@users.noreply.github.com>
2023-05-29 03:44:28 -0700

    Add files via upload

--- gsea/gsea_barplot_program.R
+++ gsea/gsea_barplot_program.R
@@ -32,6 +32,8 @@ parser$add_argument("-e", "--event", type="character", required=TRUE, help="Spec
 parser$add_argument("-s", "--set", type="character", required=TRUE, help="Gene set from GSEA") 
 parser$add_argument("-p", "--pval", type="character", required=TRUE, help="Specify p-value to use as threshold (i.e. Adjusted P-value, FDR q-val, etc.)") 
 parser$add_argument("-t", "--threshold", required=TRUE, help="P-value threshold for visualization")
+parser$add_argument("-height", "--height_param", required=TRUE, type="integer", help="Figure panel height")
+parser$add_argument("-width", "--width_param", required=TRUE, type="integer", help="Figure panel width")
 parser$add_argument("-m", "--method", type="character", required=TRUE, help="GSEA method used for analysis (i.e. prerank or enrichment)")
 parser$add_argument("-g", "--group", type="logical", default=FALSE, required=FALSE, help="Specify group analysis if comparing multiple gene sets")
 parser$add_argument("-o", "--output", type="character", required=TRUE, help="Path to output directory")   
@@ -46,49 +48,52 @@ pval_name = args$pval[1]
 set = args$set[1]
 threshold = args$threshold[1]
 method = args$method[1]
+height = args$height_param[1]
+width = args$width_param[1]
 group = args$group[1]
 output = args$output[1]
 
 
-if (group==FALSE) {
+if (group==FALSE & 
+    !is.null(input.file) & 
+    is.null(input.file2) & 
+    is.null(input.file3) & 
+    is.null(input.file4)) {
 
   input.data = data.table::fread(input.file)
 
 } else if (group==TRUE & 
+           !is.null(input.file) & 
            !is.null(input.file2) & 
            is.null(input.file3) & 
-           is.null(input.file4)
-           ) {
+           is.null(input.file4)) {
 
   temp.data = data.table::fread(input.file)
   input.data2 = data.table::fread(input.file2)
   input.data = rbind(temp.data, input.data2)
-  #input.data = input.data[order(input.data$Gene_set), ]
 
 } else if (group==TRUE & 
+           !is.null(input.file) & 
            !is.null(input.file2) & 
            !is.null(input.file3) & 
-           is.null(input.file4)
-           ) {
+           is.null(input.file4)) {
 
   temp.data = data.table::fread(input.file)
   input.data2 = data.table::fread(input.file2)
   input.data3 = data.table::fread(input.file3)
   input.data = rbind(temp.data, input.data2, input.data3)
-  #input.data = input.data[order(input.data$Gene_set), ]
 
 } else if (group==TRUE & 
-           !is.null(input.file2) & 
+           !is.null(input.file) & 
+           !is.null(input.file2) &
            !is.null(input.file3) & 
-           !is.null(input.file4)
-           ) {
+           !is.null(input.file4)) {
 
   temp.data = data.table::fread(input.file)
   input.data2 = data.table::fread(input.file2)
   input.data3 = data.table::fread(input.file3)
   input.data4 = data.table::fread(input.file4)
   input.data = rbind(temp.data, input.data2, input.data3, input.data4)
-  #input.data = input.data[order(input.data$Gene_set), ]
 
 }
 
@@ -121,8 +126,7 @@ clean_data = function(x,
 	temp.data = temp.data[!is.na(as.numeric(as.character(temp.data$pvalue))), ]
 	temp.data$pvalue = as.numeric(temp.data$pvalue)
 	temp.data = temp.data[!(temp.data$pvalue %in% c("pvalue")), ]
-  sig.data = temp.data %>% 
-              dplyr::filter(pvalue < as.numeric(threshold))
+  sig.data = temp.data %>% dplyr::filter(pvalue < as.numeric(threshold))
   
   if (nrow(sig.data)==0) { 
     stop("No significant terms found.")
@@ -132,57 +136,17 @@ clean_data = function(x,
 }
 
 ######################################################################################
-### Convert values to scientific notations
-######################################################################################
-
-convert2scientific = function(x) { 
-
-    out = gsub("1e", "10^{", format(x), fixed=TRUE)
-    out = gsub("{-0", "{-", out, fixed=TRUE)
-    out = gsub("{+", "{", out, fixed=TRUE)
-    out = gsub("{0", "{", out, fixed=TRUE)
-    out = paste(out, "}", sep="")
-    
-    return(parse(text=out))
-}
-
-convert_x_log10 = function(name=NULL, 
-                           power=seq(-10, 20), 
-                           ...
-                           ) {
-
-    breaks10 = 10^power
-    scale_x_log10(name, 
-                  breaks=breaks10, 
-                  labels=convert2scientific(breaks10), 
-                  ...
-                  )
-}
-
-convert_y_log10 = function(name=NULL, 
-                           power=seq(-10, 20), 
-                           ...
-                           ) { 
-
-    breaks10 = 10^power 
-    scale_y_log10(name, 
-                  breaks=breaks10, 
-                  labels=convert2scientific(breaks10), 
-                  ...
-                  )
-}
-
-######################################################################################
 ### Main function
 ######################################################################################
-
 
 compute_barplot = function(x,
 						               event,
                            set,
                            pval_name,
                            group,
-                           method, 
+                           method,
+                           height,
+                           width,  
 						               output
                            ) {
 
@@ -195,32 +159,30 @@ compute_barplot = function(x,
     }
 
     plot = ggplot(temp_df, aes(x=-log10(pvalue), 
-                               y=reorder(term, -log10(pvalue)), 
+                               y=reorder(source, -log10(pvalue)), 
                                fill=.data[[feature]])) + 
-            geom_bar(stat="identity", position=position_dodge(width=0.9), 
-                     width=0.8, linewidth=1.5, color="black") + 
-            theme_classic(base_size=20, base_family="", 
-                          base_line_size=1.5, base_rect_size=2.0) +
-            theme(legend.title = element_text(color="black", size=20),
-                  legend.position = ifelse(group==TRUE, "right", "none"),
-                  legend.text = element_text(color="black", size=20),
-                  axis.text = element_text(color="black", size=20),
-                  axis.title.x = element_text(color="black", size=20),
-                  axis.title.y = element_blank()) +
-            labs(x=paste0(pval_name, " (-log10)")) +
-            scale_fill_manual(values = c("#F3A583", "#3C93C2", "#6CBA7D", "#F2AD00")) + 
-            scale_x_continuous(expand = c(0, 0)) 
+              geom_bar(stat="identity", position=position_dodge(width=0.9), 
+                    width=0.8, linewidth=1.5, color="black", 
+                    fill="#ECCBAE") + 
+              theme_classic(base_size=20, base_family="", 
+                    base_line_size=1.5, base_rect_size=2.0) +
+              theme(legend.title = element_text(color="black", size=20),
+                    legend.position = ifelse(group==TRUE, "right", "none"),
+                    legend.text = element_text(color="black", size=20),
+                    axis.text = element_text(color="black", size=20),
+                    axis.title.x = element_text(color="black", size=20),
+                    axis.title.y = element_blank()) +
+              labs(x=paste0(pval_name, " (-log10)")) +
+              scale_fill_manual(values = c("#F3A583", "#3C93C2", "#6CBA7D", "#F2AD00")) 
 
     png(paste0(output, paste0(event, "_", set, "_terms_barplot.png")),
-        width=ifelse(group==TRUE, 20, 11),
-        height=ifelse(group==TRUE, 18, 7),
-        units="in", res=600)
+        width=width, height=height, units="in", res=600)
     print(plot)
     dev.off()
 
 }
 
 compute_barplot(input.data, event, set, pval_name, 
-                group, method, output)
+                group, method, height, width, output)
 
 
